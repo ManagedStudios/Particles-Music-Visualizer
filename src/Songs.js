87b5bad2ac563b25b68c3bd2b2df802ac70e0659
ManagedStudios <dibbomrinmoy@hotmail.com>
2024-06-11 18:53:32 +0200

    completed

--- src/Songs.js
+++ src/Songs.js
@@ -16,7 +16,6 @@ export default class Songs {
 
   static Day__N__Nite___Original_Hip_Hop_Mix_iK19GNLmoWk_mp3 = `./src/audio_files/Day ’N’ Nite - Original Hip Hop Mix-iK19GNLmoWk.mp3`
 
-  static Disturbia_wmGjajXDHKo_mp3 = `./src/audio_files/Disturbia-wmGjajXDHKo.mp3`
 
   static Drake___Hotline_Bling_uxpDa_c_4Mc_mp3 = `./src/audio_files/Drake - Hotline Bling-uxpDa-c-4Mc.mp3`
 
@@ -60,8 +59,6 @@ export default class Songs {
 
   static OneRepublic___Counting_Stars_hT_nvWreIhg_mp3 = `./src/audio_files/OneRepublic - Counting Stars-hT_nvWreIhg.mp3`
 
-  static OneRepublic___Didn_t_I__Official_Music_Video__OPGtpO1g_jk_mp3 = `./src/audio_files/OneRepublic - Didn’t I (Official Music Video)-OPGtpO1g_jk.mp3`
-
   static PSY___GANGNAM_STYLE________M_V_9bZkp7q19f0_mp3 = `./src/audio_files/PSY - GANGNAM STYLE(강남스타일) M_V-9bZkp7q19f0.mp3`
 
   static Pray_For_Me_T0pYq_Saf7g_mp3 = `./src/audio_files/Pray For Me-T0pYq_Saf7g.mp3`
@@ -72,19 +69,13 @@ export default class Songs {
 
   static Rihanna___Disturbia__Leezardz_Remix__u9efWrWHT_E_mp3 = `./src/audio_files/Rihanna - Disturbia (Leezardz Remix)-u9efWrWHT_E.mp3`
 
-  static Roddy_Ricch___The_Box__Official_Music_Video__UNZqm3dxd2w_mp3 = `./src/audio_files/Roddy Ricch - The Box [Official Music Video]-UNZqm3dxd2w.mp3`
-
   static Sheck_Wes___Mo_Bamba__Official_Music_Video__VWoIpDVkOH0_mp3 = `./src/audio_files/Sheck Wes - Mo Bamba (Official Music Video)-VWoIpDVkOH0.mp3`
 
   static The_Business__Vintage_Culture___Dubdogz_Remix__D4KdvQNiSlk_mp3 = `./src/audio_files/The Business (Vintage Culture & Dubdogz Remix)-D4KdvQNiSlk.mp3`
 
   static Trapped_in_8Bit_Uo8lX9sokwk_mp3 = `./src/audio_files/Trapped in 8Bit-Uo8lX9sokwk.mp3`
 
-  static Travis_Scott___HIGHEST_IN_THE_ROOM__Official_Music_Video__tfSS1e3kYeo_mp3 = `./src/audio_files/Travis Scott - HIGHEST IN THE ROOM (Official Music Video)-tfSS1e3kYeo.mp3`
-
   static Try_Everything_8kd2BLUX2bQ_mp3 = `./src/audio_files/Try Everything-8kd2BLUX2bQ.mp3`
-
-  static Tyga___Taste__Official_Video__ft__Offset_LjxulQ1bEWg_mp3 = `./src/audio_files/Tyga - Taste (Official Video) ft. Offset-LjxulQ1bEWg.mp3`
 
   static WHERE_are_you_now_remix_MP3 = `./src/audio_files/WHERE are you now remix.MP3`
 
@@ -94,22 +85,69 @@ export default class Songs {
 
   static Where_Are_You_Now__23UKjwu_10_mp3 = `./src/audio_files/Where Are You Now--23UKjwu_10.mp3`
 
-  static alan_walker_ina_wroldsen_blue_official_lyric_video___mp3 = `./src/audio_files/alan-walker-ina-wroldsen-blue-official-lyric-video- .mp3`
-
   static clean_bandit_rather_be_feat_jess_glynne_robin_schulz_remix___mp3 = `./src/audio_files/clean-bandit-rather-be-feat-jess-glynne-robin-schulz-remix- .mp3`
 
   static david_guetta_lovers_on_the_sun_lyrics_video_ft_sam_martin___mp3 = `./src/audio_files/david-guetta-lovers-on-the-sun-lyrics-video-ft-sam-martin- .mp3`
 
-  static greedy__QtN6qocpKU_mp3 = `./src/audio_files/greedy-_QtN6qocpKU.mp3`
 
-  static mapped_filenames_txt = `./src/audio_files/mapped_filenames.txt`
+
+
 
   static will_i_am___Scream___Shout_ft__Britney_Spears_kYtGl1dX5qI_mp3 = `./src/audio_files/will.i.am - Scream & Shout ft. Britney Spears-kYtGl1dX5qI.mp3`
 
+  static Alan_Walker_x_Imanbek_Sweet_Dreams = `./src/audio_files/Alan Walker x Imanbek - Sweet Dreams (Official Music Video)-SM-BT9cijI4.mp3`
+  static Rita_ORA_David_Guetta_Imanbek_BIG_ft_Gunna = `./src/audio_files/RITA ORA,  DAVID GUETTA, IMANBEK – BIG FT. GUNNA [OFFICIAL VIDEO]-Fe-GrSOrg1o.mp3`
+
+
   static songList = [
+    Songs.Goosebumps_mp3,
+    Songs.ROCKSTAR_h0w5g0p9RsM_mp3,
+    Songs.Right_Round__feat__Ke_ha__WUu0YvPX69E_mp3,
+    Songs.I_Follow_Rivers___Lykke_Li__La_Vie_d_Adèle_La_Vida_de_Adele__K3JGxj2rvAs_mp3,
+    Songs.I_m_Good__Blue___Oliver_Heldens_Remix__vCxxSUWzZRI_mp3,
+    Songs.In_the_Dark_SQz58RLQqgE_mp3,
+    Songs.Johnny_Orlando___Everybody_Wants_You_jiXP_FRrSOg_mp3,
+    Songs.Sheck_Wes___Mo_Bamba__Official_Music_Video__VWoIpDVkOH0_mp3,
+    Songs.Lil_Nas_X___STAR_WALKIN___League_of_Legends_Worlds_Anthem__HYsz1hP0BFo_mp3,
+    Songs.Baby_Don_t_Hurt_Me_0zFzJg2srvQ_mp3,
+    Songs.Haddaway___What_Is_Love__Official__HEXWRTEbj1I_mp3,
+    Songs.Mirage__for_Assassin_s_Creed_Mirage__VQYpJc8iq2g_mp3,
+    Songs.Hello_Ei8UnOPJX7w_mp3,
+    Songs.The_Business__Vintage_Culture___Dubdogz_Remix__D4KdvQNiSlk_mp3,
+    Songs.Day__N__Nite___Original_Hip_Hop_Mix_iK19GNLmoWk_mp3,
+    Songs.Come_2gether_Hk5C6RpJ5N4_mp3,
+    Songs.clean_bandit_rather_be_feat_jess_glynne_robin_schulz_remix___mp3,
+    Songs.Try_Everything_8kd2BLUX2bQ_mp3,
+    Songs.Waka_Waka__Esto_es_Africa__1FeEa2Ew2yo_mp3,
+    Songs.david_guetta_lovers_on_the_sun_lyrics_video_ft_sam_martin___mp3,
+    Songs.Everyday_NPDQYPL3uWw_mp3,
+    Songs.Madcon___Don_t_Worry_feat__Ray_Dalton__Official_Video__y74UPiaK7u0_mp3,
+    Songs.Drake___Hotline_Bling_uxpDa_c_4Mc_mp3,
     Songs.ACRAZE___Do_It_To_It__Ft__Cherish__6wwuEXlIniU_mp3,
     Songs.Am_I_Dreaming_P_X_iHTe5PI_mp3,
+    Songs.Astronaut_In_The_Ocean_5_FGxUINL_Q_mp3,
+    Songs.Close_To_Me__ESLe5Ub0IE_mp3,
+    Songs.Fading_Xp7LPr2QHbM_mp3,
+    Songs.Niagara_Falls__Foot_or_2__SFMCCPX5TM4_mp3,
+    Songs.Numb_7GC7eI9c_20_mp3,
+    Songs.Mood__Remix__e_9zmBhCfmk_mp3,
+    Songs.Where_Are_You_Now__23UKjwu_10_mp3,
+    Songs.WHERE_are_you_now_remix_MP3,
+    Songs.OneRepublic___Counting_Stars_hT_nvWreIhg_mp3,
+    Songs.Jason_Derulo___Tip_Toe_feat__French_Montana__Official_Music_Video__nNA9ru2Ox5o_mp3,
+    Songs.Kernkraft_400__A_Better_Day__0_9BSGgyshM_mp3,
+    Songs.Rihanna___Disturbia__Leezardz_Remix__u9efWrWHT_E_mp3,
+    Songs.Pray_For_Me_T0pYq_Saf7g_mp3,
+    Songs.PSY___GANGNAM_STYLE________M_V_9bZkp7q19f0_mp3,
+    Songs.Trapped_in_8Bit_Uo8lX9sokwk_mp3,
+    Songs.On_My_Own_Qm4s0OQGFFI_mp3,
+    Songs.Alan_Walker_x_Imanbek_Sweet_Dreams,
+    Songs.Metro_Boomin__Travis_Scott___Raindrops__Insane___Visualizer__2WqI50mc7Jo_mp3,
     Songs.will_i_am___Scream___Shout_ft__Britney_Spears_kYtGl1dX5qI_mp3,
-    Songs.clean_bandit_rather_be_feat_jess_glynne_robin_schulz_remix___mp3
+    Songs.Rita_ORA_David_Guetta_Imanbek_BIG_ft_Gunna,
+    Songs.Wavin__Flag__Coca_Cola__Celebration_Mix__cc4og_BpscM_mp3
+
+
+
   ]
 }
